a260a57716e62e88e1fb876b6fcfb412daf1f10d
Bhuvan Teki <bhuvanteki@gmail.com>
2026-02-26 23:23:17 +0530

    Update day_1_problems.py

--- Python/day_1_problems.py
+++ Python/day_1_problems.py
@@ -1,19 +1,17 @@
 # Question 1: Basic String Input
-# input = Bhuvan
-# input = 18
+# Sample Input: Bhuvan, 18
+# Sample Output: Hello Bhuvan, you are 18 years old.
 
 name = input()
 age = input()
 statement = ("Hello " + name + ", you are " + age + " years old.")
 print(statement)
 
-# output: Hello Bhuvan, you are 18 years old.
-
 # --------------------------------------------------
 
 # Question 2: Numbers + Thinking (Sum & Product)
-# input = 10
-# input = 5
+# Sample Input: 10, 5
+# Sample Output: Sum: 15, Product: 50
 
 num1 = int(input())
 num2 = int(input())
@@ -24,29 +22,23 @@ product = num1 * num2
 print("Sum:", sum)
 print("Product:", product)
 
-# output:
-# Sum: 15
-# Product: 50
-
 # --------------------------------------------------
 
 # Question 3: Input + Type + Logic (Future Age)
-# input = 18
+# Sample Input: 18
+# Sample Output: After 5 years, you will be 23 years old.
 
 age = int(input())
 new_age = age + 5
 statement = "After 5 years, you will be " + str(new_age) + " years old."
 print(statement)
 
-# output: After 5 years, you will be 23 years old.
-
 # --------------------------------------------------
 
 # Question 4: Money + Real Life Logic (GST Calculation)
-# input = 100
+# Sample Input: 100
+# Sample Output: Final Price: 118.0
 
 price = float(input())
 final_price = price + (price * 18 / 100)
 print("Final Price:", final_price)
-
-# output: Final Price: 118.0
