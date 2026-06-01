8e17ea76605fc576ea59eada60b9352eb37a9a2b
TEKI BHUVANA SRI MANIKANTA SAI CHARAN <bhuvanteki@gmail.com>
2026-06-02 04:36:51 +0530

    Create D1.py

---
+++ Python/D1.py
@@ -0,0 +1,20 @@
+# Problem 1
+print("Hello World")
+
+# Problem 2
+print("My name is Bhuvan")
+
+# Problem 3
+print(2 + 5)
+
+# Problem 4
+print(10 - 3)
+
+# Problem 5
+print(4 * 6)
+
+# Problem 6
+print(20 / 5)
+
+# Problem 7
+print("2 + 5")
